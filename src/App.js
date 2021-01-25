17a9fac84ee9728a267f4d82d5c3d5eaf2e9c6d4
Giovanna Betti <giovannabetti@gmail.com>
2021-01-24 23:21:37 -0300

    requisite 02 completed - use of shouldComponentUpdate and componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -14,17 +14,31 @@ class App extends React.Component {
     this.fetchDog();
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.data.message.includes("terrier")) {
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("dogURL", JSON.stringify(this.state.data.message));
+    const dogBreed = this.state.data.message.split("/")[4];
+    alert(dogBreed);
+  }
+
   fetchDog() {
     fetch("http://dog.ceo/api/breeds/image/random")
       .then(response => response.json())
       .then(result => this.setState({ data: result }));
   }
+
   render() {
     if (this.state.data === "") return "Loading...";
     return (
       <main>
         <h3>Doguíneos</h3>
-        <button onClick={this.fetchDog}>Novo Doguíneo</button>
+        <button type="button" onClick={this.fetchDog}>Novo Doguíneo</button>
         <section>
           <img src={this.state.data.message} alt="Imagem aleatória de um cachorro" />
         </section>
