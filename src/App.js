4bb14e4a11e6eb24e03baa5b06760b81a6f6a176
Giovanna Betti <giovannabetti@gmail.com>
2021-01-24 23:12:40 -0300

    almost done - exercise of componentDidMount use

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  render() {
-    return (
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ""
+    };
+    this.fetchDog = this.fetchDog.bind(this);
+  }
 
-    )
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  fetchDog() {
+    fetch("http://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(result => this.setState({ data: result }));
+  }
+  render() {
+    if (this.state.data === "") return "Loading...";
+    return (
+      <main>
+        <h3>Doguíneos</h3>
+        <button onClick={this.fetchDog}>Novo Doguíneo</button>
+        <section>
+          <img src={this.state.data.message} alt="Imagem aleatória de um cachorro" />
+        </section>
+      </main>
+    );
   }
 }
 
